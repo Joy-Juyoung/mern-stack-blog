aabd5289cdd31a102eede6f2f71ae469411ebad0
Juyoung <ljy9407@gmail.com>
2025-01-01 20:55:13 -0700

    Added react router dom and made pages for routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+import HomePage from "./routes/Homepage.jsx";
+import LoginPage from "./routes/LoginPage.jsx";
+import RegisterPage from "./routes/RegisterPage.jsx";
+import PostListPage from "./routes/PostListPage.jsx";
+import SinglePostPage from "./routes/SinglePostPage.jsx";
+import WritePage from "./routes/WritePage.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+  {
+    path: "/posts",
+    element: <PostListPage />,
+  },
+  {
+    path: "/:slug",
+    element: <SinglePostPage />,
+  },
+  {
+    path: "/write",
+    element: <WritePage />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
